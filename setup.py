541b51d7b7c1774c74c9586b5757e2d86e14ebeb
martianrock <martianrock@users.noreply.github.com>
2016-12-05 00:52:02 -0500

    Update setup.py
    copy-paste error fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="vadim.bich@gmail.com",
     url="https://github.com/martianrock/django-simple-auth-checker/",
     description="Simple Nginx's auth_request handler in Django",
-    long_description="Simple django app to check for request.user.is_staff/is_superuser and r
+    long_description="Simple django app to check for request.user.is_staff/is_superuser and respond with appropriate HTTP code. This can be used as nginx's auth_request handler to protect non-django resources with normal django authentication process.",
     license = "MIT",
     platforms = ["any"],
     packages=['simple_auth_checker']
